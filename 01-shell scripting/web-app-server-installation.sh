2dd46bd36d16bdb75ad5bde453dfe101783d04d0
Anuragp <Anurag>
2020-07-11 13:34:10 +0530

    --- Auto-Git Commit ---

--- 01-shell scripting/web-app-server-installation.sh
+++ 01-shell scripting/web-app-server-installation.sh
@@ -2,4 +2,12 @@ HEADING(){
     echo -e "\e[33m$1\e[0m\n"
 }
 
+STATUS() {
+    if [ $? == 0 ]
+     then
+       echo "$1 
+}
 HEADING "WEBSERVER-INSTALLATION"
+
+yum install nginx -y &> /var/log/app.log
+
