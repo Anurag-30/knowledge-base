ce0798d0ed2c13a9eb89fdfd9cac1988d1a975b1
Anuragp <Anurag>
2020-07-13 10:35:56 +0530

    --- Auto-Git Commit ---

--- 01-shell scripting/web-app-server-installation.sh
+++ 01-shell scripting/web-app-server-installation.sh
@@ -43,6 +43,7 @@ STATUS $? "DOWNLOADING TOMCAT"
 chown $APPUSER:$APPUSER /home/$APPUSER -R 
 curl -s https://raw.githubusercontent.com/Anurag-30/knowledge-base/master/01-shell%20scripting/tomcat.service -o /etc/systemd/system/tomcat.service
 chmod +x /etc/systemd/system/tomcat.service
+STATUS_CHECK $? "Reload Systemd Daemon"
 
 systemctl daemon-reload &>>/dev/null
 STATUS_CHECK $? "Reload Systemd Daemon"
